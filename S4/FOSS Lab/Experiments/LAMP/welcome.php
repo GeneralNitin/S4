8e928b40681410b8deabbc75ee72fd81e6012a5e
Srividya <srividya.krishnakumar99@gmail.com>
2019-05-05 23:05:44 +0000

    Modified php files - fosslab

--- S4/FOSS Lab/Experiments/LAMP/welcome.php
+++ S4/FOSS Lab/Experiments/LAMP/welcome.php
@@ -1,13 +1,14 @@
 <?php
-include('session.php');
+//include('session.php');
+include("config.php");
+session_start();
 ?>
-<html">
-<head>
-<title>Welcome </title>
-</head>
-<body>
-
-<h1>Welcome <?php echo $login_session; ?></h1>
-<h2><a href = "logout.php">Sign Out</a></h2>
-</body>
+<html>
+	<head>
+		<title>Welcome</title>
+	</head>
+	<body>
+		<h1>Welcome <?php echo "username"; ?></h1>
+		<h2><a href = "logout.php">Sign Out</a></h2>
+	</body>
 </html>
